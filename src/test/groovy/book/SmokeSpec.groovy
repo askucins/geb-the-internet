6e247caa8597fce250d03fb1e826c99a597583d4
Adam Skucinski <askucins@gmail.com>
2017-11-01 21:12:22 +0100

    minimal example with screenshots

--- src/test/groovy/book/SmokeSpec.groovy
+++ src/test/groovy/book/SmokeSpec.groovy
@@ -1,9 +1,27 @@
 package book
 
-import groovy.util.logging.Slf4j
+import org.slf4j.LoggerFactory
+import spock.lang.Shared
 import spock.lang.Specification
+import geb.Browser
 
-@Slf4j
+// TODO @Slf4j not used b/c https://github.com/spockframework/spock/issues/491
 class SmokeSpec extends Specification {
+    @Shared
+    def log = LoggerFactory.getLogger(this.class)
 
+    def setupSpec() {
+        log.info "Select an alternative chromedriver"
+        System.properties.'webdriver.chrome.driver' = '/opt/webdriver/chromedriver'
+        log.info System.properties.'geb.build.reportsDir'.toString()
+    }
+
+    def "should open the Gebish page"() {
+        expect:
+        Browser.drive {
+            go "http://gebish.org"
+            report "Geb-home-page"
+            assert title == "Geb - Very Groovy Browser Automation"
+        }
+    }
 }
