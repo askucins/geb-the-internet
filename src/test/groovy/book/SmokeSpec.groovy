a8ad985f904792310d0c5b29c675bc2aa4b3040e
Adam Skucinski <askucins@gmail.com>
2017-11-01 22:48:44 +0100

    added properties fallback to cover execution from Gradle and from Spock (via IDE)

--- src/test/groovy/book/SmokeSpec.groovy
+++ src/test/groovy/book/SmokeSpec.groovy
@@ -4,16 +4,22 @@ import org.slf4j.LoggerFactory
 import spock.lang.Shared
 import spock.lang.Specification
 import geb.Browser
+import spock.util.environment.RestoreSystemProperties
 
 // TODO @Slf4j not used b/c https://github.com/spockframework/spock/issues/491
+@RestoreSystemProperties
 class SmokeSpec extends Specification {
     @Shared
     def log = LoggerFactory.getLogger(this.class)
 
     def setupSpec() {
-        log.info "Select an alternative chromedriver"
-        System.properties.'webdriver.chrome.driver' = '/opt/webdriver/chromedriver'
-        log.info System.properties.'geb.build.reportsDir'.toString()
+        def sp = System.properties
+
+        sp.'webdriver.chrome.driver' = sp.'webdriver.chrome.driver' ?: '/opt/webdriver/chromedriver'
+        log.info "Path to chromedriver: ${sp.'webdriver.chrome.driver'}"
+
+        sp.'geb.build.reportsDir' = sp.'geb.build.reportsDir' ?: "reports"
+        log.info "Additional Geb reports in: ${sp.'geb.build.reportsDir'}"
     }
 
     def "should open the Gebish page"() {
@@ -24,4 +30,5 @@ class SmokeSpec extends Specification {
             assert title == "Geb - Very Groovy Browser Automation"
         }
     }
+
 }
