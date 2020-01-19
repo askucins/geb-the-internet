8349ff2be5e78064faf78349d032c7b9fe8a7314
Adam Skucinski <askucins@gmail.com>
2020-01-19 14:16:22 +0100

    misc stuff from GebBook

--- src/integration/groovy/org/askucins/gebbook/local/SmokeSpec.groovy
+++ src/integration/groovy/org/askucins/gebbook/local/SmokeSpec.groovy
@@ -6,8 +6,12 @@ import groovy.util.logging.Slf4j
 class SmokeSpec extends GebLocalSpec {
 
     def "should open page"() {
-        expect:
+        when:
         to SmokePage
+        then:
+        heading.size() == 2
+        and:
+        heading*.text() == ['This is heading.','This is another heading.']
         cleanup:
         report("Smoke-page")
     }
--- src/main/groovy/org/askucins/gebbook/local/SmokePage.groovy
+++ src/main/groovy/org/askucins/gebbook/local/SmokePage.groovy
@@ -6,5 +6,7 @@ import groovy.util.logging.Slf4j
 class SmokePage extends GebLocalPage {
     static url = 'smoke.html'
     static at = { title == "Geb Local" }
-    static content = {}
+    static content = {
+        heading {$("h2", class:"heading")}
+    }
 }
