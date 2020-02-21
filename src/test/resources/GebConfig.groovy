e84cbaf2a57ac1b149dd1f15248df9e84d87c427
Adam Skucinski <askucins@gmail.com>
2020-02-21 18:15:02 +0100

    misc

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -2,11 +2,15 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 import static org.askucins.utils.CustomizedChromeDriver.chromeDriver
+import static org.askucins.utils.CustomizedFirefoxDriver.firefoxDriver
 import static org.askucins.utils.TestDriver.*
 
 switch (System.getProperty('org.askucins.webdriver')) {
     case FIREFOX.toString():
         driver = { new FirefoxDriver() }
+        break
+    case FIREFOXHEADLESS.toString():
+        driver = { firefoxDriver([headless: true]) }
         break
     case CHROME.toString():
         driver = { chromeDriver([headless: false]) }
