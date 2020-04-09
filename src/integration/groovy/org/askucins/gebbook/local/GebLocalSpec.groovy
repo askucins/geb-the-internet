b763db279edad0987fbc641c04dfb2fe2f27a2cf
Adam Skucinski <askucins@gmail.com>
2020-04-09 20:48:43 +0200

    added temporary workaround for JUnit4 Rules in Spock

--- src/integration/groovy/org/askucins/gebbook/local/GebLocalSpec.groovy
+++ src/integration/groovy/org/askucins/gebbook/local/GebLocalSpec.groovy
@@ -3,16 +3,25 @@ package org.askucins.gebbook.local
 import geb.driver.CachingDriverFactory
 import geb.spock.GebReportingSpec
 import groovy.util.logging.Slf4j
+import org.junit.Rule
+import org.junit.rules.TestName
 import spock.util.environment.RestoreSystemProperties
 
 @Slf4j
 @RestoreSystemProperties
 abstract class GebLocalSpec extends GebReportingSpec {
+    @Rule
+    TestName testName = new TestName()
+
     def setupSpec() {
         assert System.getProperty('geb.env')
         log.info "geb.env: {}", System.getProperty('geb.env')
         assert System.getProperty('geb.build.reportsDir')
         log.info "geb.build.reportsDir: {}", System.getProperty('geb.build.reportsDir')
+    }
+
+    def setup() {
+        log.info ">>>> ${testName.methodName}"
     }
 
     def cleanupSpec() {
