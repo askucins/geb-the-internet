1938ac66c901806230a2c429bde4e65407364d99
Adam Skucinski <askucins@gmail.com>
2020-05-25 22:50:26 +0200

    misc changes

--- src/integration/groovy/org/askucins/gebbook/local/PageWithDivSpec.groovy
+++ src/integration/groovy/org/askucins/gebbook/local/PageWithDivSpec.groovy
@@ -1,24 +1,28 @@
 package org.askucins.gebbook.local
 
 import geb.Browser
+import geb.error.ContentCountOutOfBoundsException
+import geb.error.RequiredPageContentNotPresent
 import groovy.util.logging.Slf4j
+import spock.lang.Ignore
 
 @Slf4j
 class PageWithDivSpec extends GebLocalSpec {
 
-    def "should open page-with-div page"() {
-        expect:
-        to PageWithDivPage
-        cleanup:
-        report "PageWithDiv"
-    }
-
+    @Ignore
     def "should find the div via 'drive'"() {
         expect:
         Browser.drive {
             to PageWithDivPage
             assert theDiv.text() == 'aa'
         }.quit()
+    }
+
+    def "should open page-with-div page"() {
+        expect:
+        to PageWithDivPage
+        cleanup:
+        report "PageWithDiv"
     }
 
     def "should work with various content templates"() {
@@ -29,10 +33,78 @@ class PageWithDivSpec extends GebLocalSpec {
         and:
         theDiv().text() == 'aa' // as a method
         and:
-        theDivText == 'aa'
+        theDivText == 'aa' //internal reference to another content definition
         and:
         theDivTemplate('a').text() == 'aa' // returns Navigator
         and:
         theDivTemplateText('a') == 'aa' //returns text
+        and:
+        theDivByProperty.text() == 'aa'
+        and:
+        theDivByMethod.text() == 'aa'
+    }
+
+    def "should apply 'required' option"() {
+        when:
+        to PageWithDivPage
+        then:
+        !notRequiredDiv
+
+        when:
+        requiredDiv
+        then:
+        RequiredPageContentNotPresent error = thrown()
+    }
+
+    def "should apply 'min' option"() {
+        when:
+        to PageWithDivPage
+        then:
+        paragraphsEnough
+
+        when:
+        paragraphsNotEnough
+        then:
+        ContentCountOutOfBoundsException error = thrown()
+    }
+
+    def "should apply 'max' option"() {
+        when:
+        to PageWithDivPage
+        then:
+        paragraphsNotTooMany
+
+        when:
+        paragraphsTooMany
+        then:
+        ContentCountOutOfBoundsException error = thrown()
+    }
+
+    def "should apply 'times' option"() {
+        when:
+        to PageWithDivPage
+        then:
+        paragraphsWithinLimit
+
+        when:
+        paragraphsOutsideLimit
+        then:
+        ContentCountOutOfBoundsException error = thrown()
+    }
+
+    def "should apply 'cache' option"() {
+        when:
+        to PageWithDivPage
+        then:
+        valueCached == 1
+        and:
+        valueNotCached == 1
+
+        when:
+        value = 2 // Gotcha! This will be evaluated within page, so the property will be changed
+        then:
+        valueCached == 1
+        and:
+        valueNotCached == 2
     }
 }
--- src/main/groovy/org/askucins/gebbook/local/PageWithDivPage.groovy
+++ src/main/groovy/org/askucins/gebbook/local/PageWithDivPage.groovy
@@ -8,11 +8,34 @@ class PageWithDivPage extends GebLocalPage {
     static url = 'page-with-div.html'
     static content = {
         theDiv { $('div', id: 'a') }
-        theDivText {theDiv.text()}
+        theDivText { theDiv.text() }
         theDivTemplate { id -> $('div', id: id) }
         theDivTemplateText { id -> theDivTemplate(id).text() }
+        theDivByProperty { $('div', id: divId) }
+        theDivByMethod { $('div', id: divId()) }
+
+        requiredDiv { $('div', id: 'b') }
+        notRequiredDiv(required: false) { $('div', id: 'b') }
+
+        paragraphsNotEnough(min: 3) { $('div.paragraphs p') }
+        paragraphsEnough(min: 2) { $('div.paragraphs p') }
+
+        paragraphsTooMany(max: 1) { $('div.paragraphs p') }
+        paragraphsNotTooMany(max: 2) { $('div.paragraphs p') }
+
+        paragraphsWithinLimit(times: 2..3) { $('div.paragraphs p') }
+        paragraphsOutsideLimit(times: 3..3) { $('div.paragraphs p') }
+
+        valueCached(cache: true) { value }
+        valueNotCached { value }
     }
     static at = {
         title == 'Page with div'
     }
+
+    def value = 1
+
+    def divId = 'a'
+
+    def divId() { 'a' }
 }
